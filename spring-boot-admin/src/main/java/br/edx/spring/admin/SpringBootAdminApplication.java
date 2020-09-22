82afa8c9546e4055e4dffcd37a81ccd8c066401e
Eduardo <eduaalthoff@gmail.com>
2020-09-22 02:54:04 -0300

    Admin spring configure
    Change-Id: Ifee5540db2d703f404a68afe00c698939d9b823a

--- spring-boot-admin/src/main/java/br/edx/spring/admin/SpringBootAdminApplication.java
+++ spring-boot-admin/src/main/java/br/edx/spring/admin/SpringBootAdminApplication.java
@@ -3,6 +3,10 @@ package br.edx.spring.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+
+@EnableAdminServer
 @SpringBootApplication
 public class SpringBootAdminApplication {
 
